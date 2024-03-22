aeeb7dac1af038f49d6ec8c1343b37bc4df9bdd9
Franklin J. Valdez <franklinjoel1809@gmail.com>
2024-03-22 08:27:55 -0400

    Agregadando funcionalidades de publicación de imágenes, texto adicional, eliminación de publicaciones y comentarios

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,13 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LeftSection from '../Sections/LeftSection/LeftSection';
 import RightSection from '../Sections/RightSection/RightSection';
-import { RiFileGifLine} from 'react-icons/ri';
-import { FaRegCalendarAlt, FaRegHeart, FaRegComment, FaShare, FaCloudUploadAlt, FaRegImage, FaPoll, FaCalendarAlt, FaMapMarkerAlt } from 'react-icons/fa';
+import { RiFileGifLine } from 'react-icons/ri';
+import { FaRegCalendarAlt, FaRegHeart, FaRegComment, FaShare, FaCloudUploadAlt, FaRegImage, FaPoll, FaCalendarAlt, FaMapMarkerAlt, FaTimes } from 'react-icons/fa';
 import style from "./HomePage.module.css";
-import signupside from "../../assets/images/LoginImage.png";
+import signupSideImage from "../../assets/images/LoginImage.png";
 import profile from "../../assets/images/profile.png";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState([]);
+    const [newPost, setNewPost] = useState('');
+    const [newPostImage, setNewPostImage] = useState(null);
+    const [comments, setComments] = useState([]);
+    const [showCommentInput, setShowCommentInput] = useState(false);
+    const [commentText, setCommentText] = useState('');
+
+    useEffect(() => {
+        const fetchedPosts = [
+            {
+                id: 1,
+                author: 'Dariel Restituyo',
+                handle: '@restituyo',
+                time: '1 hour ago',
+                image: signupSideImage,
+                text: 'Esto es un simple post.',
+                likes: 10,
+                comments: 5,
+                shares: 3
+            }
+        ];
+        setPosts(fetchedPosts);
+    }, []);
+
+    const handlePostSubmit = () => {
+        if (newPost.trim() !== '' || newPostImage) {
+            const newPostData = {
+                id: posts.length + 1,
+                author: 'Dariel Restituyo',
+                handle: '@restituyo',
+                time: 'Just now',
+                text: newPost,
+                image: newPostImage,
+                likes: 0,
+                comments: 0,
+                shares: 0
+            };
+            setPosts([...posts, newPostData]);
+            setNewPost('');
+            setNewPostImage(null);
+        }
+    };
+
+    const handleLike = (postId) => {
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                return { ...post, likes: post.likes + 1 };
+            }
+            return post;
+        });
+        setPosts(updatedPosts);
+    };
+
+    const handleComment = (postId) => {
+        setShowCommentInput(true);
+    };
+
+    const handleCommentSubmit = (postId) => {
+        if (commentText.trim() !== '') {
+            const updatedPosts = posts.map(post => {
+                if (post.id === postId) {
+                    return { ...post, comments: post.comments + 1 };
+                }
+                return post;
+            });
+            setPosts(updatedPosts);
+            setComments([...comments, { postId, comment: commentText }]);
+            setShowCommentInput(false);
+            setCommentText('');
+        }
+    };
+
+    const handleShare = (postId) => {
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                return { ...post, shares: post.shares + 1 };
+            }
+            return post;
+        });
+        setPosts(updatedPosts);
+    };
+
+    const handleDeletePost = (postId) => {
+        const updatedPosts = posts.filter(post => post.id !== postId);
+        setPosts(updatedPosts);
+    };
+
+    const handleDeleteComment = (postId, commentIndex) => {
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                const updatedComments = comments.filter((_, index) => index !== commentIndex);
+                return { ...post, comments: post.comments - 1 };
+            }
+            return post;
+        });
+        setPosts(updatedPosts);
+        setComments(comments.filter((_, index) => index !== commentIndex));
+    };
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        setNewPostImage(file);
+    };
 
     return (
         <div className={style.homePage}>
@@ -16,11 +119,36 @@ const HomePage = () => {
                 <h2>Welcome, Dariel Restituyo</h2>
                 <div className={style.topSection}>
                     <img src={profile} alt="Profile" className={style.topprofileImage} />
-                    <input type="text" className={style.postInput} placeholder="What's on your mind?" />
-                    <button className={style.postButton}>POSTEAR</button>
+                    <input
+                        type="text"
+                        className={style.postInput}
+                        placeholder="What's on your mind?"
+                        value={newPost}
+                        onChange={(e) => setNewPost(e.target.value)}
+                    />
+                    <button className={style.postButton} onClick={handlePostSubmit}>
+                        POSTEAR
+                    </button>
+                    {newPostImage && (
+                        <div className={style.imagePreview}>
+                            <img src={URL.createObjectURL(newPostImage)} alt="Preview" />
+                            <button onClick={() => setNewPostImage(null)}>
+                                <FaTimes />
+                            </button>
+                        </div>
+                    )}
                 </div>
                 <div className={style.postIcons}>
-                    <button className={style.iconButton}><FaRegImage /></button>
+                    <label htmlFor="imageUpload" className={style.iconButton}>
+                        <FaRegImage />
+                        <input
+                            id="imageUpload"
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageUpload}
+                            style={{ display: 'none' }}
+                        />
+                    </label>
                     <button className={style.iconButton}><RiFileGifLine /></button>
                     <button className={style.iconButton}><FaPoll /></button>
                     <button className={style.iconButton}><FaCalendarAlt /></button>
@@ -32,29 +160,62 @@ const HomePage = () => {
                     <button className={style.completedButton}>For you</button>
                 </div>
                 <div className={style.publications}>
-                    <div className={style.publication}>
-                        <div className={style.profileInfo}>
-                            <img src={profile} alt="Profile" className={style.profileImage} />
-                            <div className={style.profileDetails}>
-                                <p className={style.profileName}>Dariel Restituyo</p>
-                                <p className={style.profileHandle}>@restituyo</p>
+                    {posts.map((post, index) => (
+                        <div key={post.id} className={style.publication}>
+                            <div className={style.profileInfo}>
+                                <img src={profile} alt="Profile" className={style.profileImage} />
+                                <div className={style.profileDetails}>
+                                    <p className={style.profileName}>{post.author}</p>
+                                    <p className={style.profileHandle}>{post.handle}</p>
+                                </div>
+                                <span className={style.postTime}>{post.time}</span>
+                                <button className={style.deletePostButton} onClick={() => handleDeletePost(post.id)}>
+                                    <FaTimes />
+                                </button>
                             </div>
-                            <span className={style.postTime}>1 hour ago</span>
+                            {post.image && <img src={post.image} alt="Background" className={style.image} />}
+                            <p>{post.text}</p>
+                            <div className={style.interactionButtons}>
+                                <button className={style.interactionButton} onClick={() => handleLike(post.id)}>
+                                    <FaRegHeart /> {post.likes}
+                                </button>
+                                <button className={style.interactionButton} onClick={() => handleComment(post.id)}>
+                                    <FaRegComment /> {post.comments}
+                                </button>
+                                <button className={style.interactionButton} onClick={() => handleShare(post.id)}>
+                                    <FaShare /> {post.shares}
+                                </button>
+                                <button className={style.interactionButton}><FaCloudUploadAlt /></button>
+                            </div>
+                            {showCommentInput && (
+                                <div>
+                                    <input
+                                        type="text"
+                                        placeholder="Escribe un comentario..."
+                                        value={commentText}
+                                        onChange={(e) => setCommentText(e.target.value)}
+                                        onKeyDown={(e) => {
+                                            if (e.key === 'Enter') {
+                                                handleCommentSubmit(post.id);
+                                            }
+                                        }}
+                                    />
+                                    <button onClick={() => handleCommentSubmit(post.id)}>Enviar</button>
+                                </div>
+                            )}
+                            {comments.filter(comment => comment.postId === post.id).map((comment, commentIndex) => (
+                                <div key={commentIndex} className={style.comment}>
+                                    <p>{comment.comment}</p>
+                                    <button onClick={() => handleDeleteComment(post.id, commentIndex)}>
+                                        <FaTimes />
+                                    </button>
+                                </div>
+                            ))}
                         </div>
-                        <img src={signupside} alt="Background" className={style.image} />
-                        <p>This is a sample post text.</p>
-                        <div className={style.interactionButtons}>
-                            <button className={style.interactionButton}><FaRegHeart /> 10</button>
-                            <button className={style.interactionButton}><FaRegComment /> 5</button>
-                            <button className={style.interactionButton}><FaShare /> 3</button>
-                            <button className={style.interactionButton}><FaCloudUploadAlt /></button>
-                        </div>
-                    </div>
-                    {/* Otras publicaciones */}
+                    ))}
                 </div>
                 <hr className={style.sectionSeparator} />
                 <hr className={style.sectionSeparator} />
-                {/* Más publicaciones */}
             </div>
             <RightSection />
         </div>
